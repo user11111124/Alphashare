e2012d4d142b1d3fa5ea312de3b87e710dcba9e3
David <daveman17.laptop@gmail.com>
2025-03-19 20:31:50 +0100

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ DEVELOPER_LINK = os.getenv("DEVELOPER_LINK")
 SUPPORT_LINK = os.getenv("SUPPORT_LINK")
 
 # For Koyeb/render 
-WEB_SERVER = bool(os.getenv("WEB_SERVER", True)) # make it True if deploying on koyeb/render else False
+WEB_SERVER = bool(os.getenv("WEB_SERVER", False)) # make it True if deploying on koyeb/render else False
 PING_URL = os.getenv("PING_URL") # add your koyeb/render's public url
 PING_TIME = int(os.getenv("PING_TIME")) # Add time_out in seconds
 
@@ -98,68 +98,33 @@ SUPPORTED_MIME_TYPES = [
 
 class Messages:
     START_TEXT = """
-🎉 **Welcome to {bot_name}!** 🎉
+🎉 Hello {user_mention}! 🎉
 
-Hello {user_mention}! I'm your secure file sharing assistant.
+I'm a secure file sharing assistant for members of @XTVeurope.**
 
-🔐 **Key Features:**
-• Secure File Sharing
-• Unique Download Links
-• Multiple File Types Support
-• Real-time Tracking
-• Force Subscribe
-
-📢 Join @Thealphabotz for updates!
-👨‍💻 Contact @adarsh2626 for support
-A Open Source Repo :- github.com/utkarshdubey2008/alphashare
-
-Use /help to see available commands!
+📑 Contact @XTV_official for ads!
 """
 
     HELP_TEXT = """
-📚 **Available Commands** 
-
 👤 **User Commands:**
 • /start - Start bot
 • /help - Show this help
 • /about - About bot
 
-👑 **Admin Commands:**
-• /upload - Upload file (reply to file)
-• /stats - View statistics
-• /broadcast - Send broadcast
-• Auto-Delete Feature:
-Files are automatically deleted after the set time.
-Use /auto_del to change the deletion time.
-• /short - to shorten any url in modiji 
-usage :- /short example.com
-
-An Open Source Repo :- github.com/utkarshdubey2008/alphashare
-
-⚠️ For support: @adarsh2626
+🆘 Join @XTVchat for support!
 """
 
     ABOUT_TEXT = """
 ℹ️ **About {bot_name}**
 
 **Version:** `{version}`
-**Developer:** @adarsh2626
+**Developer:** @davdxp
 **Language:** Python
 **Framework:** Pyrogram
 
-📢 **Updates:** @Thealphabotz
-🛠 **Support:** @adarsh2626
+📢 **Updates:** @XTVeurope
+🛠 **Support:** @XTVchat
 
-**Features:**
-• Secure File Sharing
-• Force Subscribe
-• Admin Controls
-• Real-time Stats
-• Multiple File Types
-• Enhanced Security
-• Automatic File Type Detection
-
-Made with ❤️ by @adarsh2626
 """
 
     FILE_TEXT = """
@@ -169,18 +134,15 @@ Made with ❤️ by @adarsh2626
 **Size:** {file_size}
 **Type:** {file_type}
 **Downloads:** {downloads}
-**Uploaded:** {upload_time}
-**By:** {uploader}
 
 🔗 **Share Link:**
 `{share_link}`
 """
 
-    FORCE_SUB_TEXT = """
+    FORCE_SUB_TEXT ℹ️ = """
 ⚠️ **Access Restricted!**
 
 Please join our channel to use this bot:
-Bot By @Thealphabotz
 
 Click button below, then try again!
 """
